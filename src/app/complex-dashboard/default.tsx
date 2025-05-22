da28c62b8f1bab69679cd3a3c1ea2f45eb7528d7
Libbna Mathew <libbna.mathew@gmail.com>
2025-05-22 16:49:54 +0530

    Intercepted Routes.

---
+++ src/app/complex-dashboard/default.tsx
@@ -0,0 +1,8 @@
+export default function DefaultDashboardLayout() {
+  return (
+    <>
+      <h1>Default Complex Dashboard</h1>
+      {/* Add your complex dashboard components here */}
+    </>
+  );
+}
